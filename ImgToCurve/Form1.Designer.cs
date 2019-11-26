cd296ea21949504c61dee3b62ee669d4b75020b0
Tengiz Margvelashvili <thetenlight@gmail.com>
2019-11-26 22:47:07 +0500

    Добавьте файлы проекта.

---
+++ ImgToCurve/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ImgToCurve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Browse_btn
+            // 
+            this.Browse_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Browse_btn.Location = new System.Drawing.Point(714, 419);
+            this.Browse_btn.Name = "Browse_btn";
+            this.Browse_btn.Size = new System.Drawing.Size(75, 23);
+            this.Browse_btn.TabIndex = 0;
+            this.Browse_btn.Text = "Обзор";
+            this.Browse_btn.UseVisualStyleBackColor = true;
+            this.Browse_btn.Click += new System.EventHandler(this.Browse_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 400);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 454);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Browse_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ImgToCurve/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgToCurve
+{
+    public partial class Form1 : Form
+    {
+        Color[] canvas = new Color[] { Color.FromArgb(255,255,255), Color.FromArgb(192, 192, 192) };   // Цвета 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_btn_Click(object sender, EventArgs e)
+        {
+            int Rows = -1;
+            int Cols = -1;
+            string text = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(file);
+                    /*
+                    string[] lines = text.Split('\n');
+                    Rows = lines.Count();
+                    if (Rows == 0) throw new FileLoadException("File is empty");
+                    M_tb.Text = Convert.ToString(Rows);
+                    Cols = lines[0].Split(';').Count();
+                    N_tb.Text = Convert.ToString(Cols - 1);
+                    GetCoordinates(lines, Rows, Cols);
+                    */
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        const int range = 120; 
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int step = 10;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            int w = bmp.Width;
+            int h = bmp.Height;
+            Color[,] pixels = new Color[w, h];
+            Color CurveColor = bmp.GetPixel(e.X, e.Y); //отладка
+            int ax = e.X - range;
+            if (ax < 0)
+                ax = 0;
+            int bx = e.X + range;
+            if (bx > w)
+                bx = w;
+            int ay = 0;
+            int by = 0;
+            if ((CurveColor != canvas[0]) && (CurveColor != canvas[1]))
+            {
+                bool IsCurveStarted = false;
+                //bool IsCurveEnded = false;
+                List<Point> points = new List<Point>();
+                for (int y = h - 1; y > 0; --y) // пробегаем по строкам (снизу вверх)
+                {
+                    var min = -1;
+                    var max = -1;
+                    //IsCurveEnded = true;   // предполагаем, что достигли конца кривой
+                    for (int x = ax; x <= bx; ++x)
+                    {
+                        var pixel = bmp.GetPixel(x, y) == CurveColor;
+                        if (pixel)
+                        {
+                            IsCurveStarted = true;
+                            if (ay == 0)
+                            {
+                                ay = y; // y координата начала нашей кривой
+                            }
+                            //IsCurveEnded = false;   // опровергаем достижение конца кривой
+
+                            if (min == -1)
+                            {
+                                max = min = x;
+                            }
+                            else if (max < x )
+                                max = x;
+                        }
+                    }
+                    if ((min != -1) && (max != -1))
+                    {
+                        /*
+                        for (int a = min; a <= max; ++a)
+                        {
+                            points.Add(new Point(a, y));
+                        }
+                        */
+                        int tmp = (max + min) / 2;
+                        points.Add(new Point(tmp, y));
+                        
+                    }
+                    if ((IsCurveStarted) && (bmp.GetPixel(ax ,y) == Color.FromArgb(160, 160, 160)))
+                    {
+                        by = y;
+                        break;
+                    }
+                    /*
+                    if ((IsCurveStarted) && (IsCurveEnded))
+                    {
+                        by = y;
+                        break;
+                    }
+                    */
+                }
+
+                BuildCurve(points);
+
+                /*
+                    int i = e.Y; i < bmp.Height; ++i)
+                {
+                    int leftBound_X = e.X - step;
+                    if (leftBound_X < 0)
+                        leftBound_X = 0;
+                    int rightBound_X = e.X - step;
+                    if (rightBound_X > bmp.Width)
+                        rightBound_X = bmp.Width;
+                    for (int j = leftBound_X; j < rightBound_X; ++j)
+                    {
+                        
+                    }
+
+                }
+                */
+            }
+
+            /*
+            for (int i = x; i < bmp.Width; ++i)
+            {
+                
+                for (int j = y-step; j < y+step; ++j)
+                {
+                    Color tmp = bmp.GetPixel(i, j);
+                    pixels[i, j] = tmp;
+                }
+            }
+            */
+        }
+        private void BuildCurve(List<Point> points)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < points.Count; ++i)
+            {
+                bmp.SetPixel(points[i].X, points[i].Y, Color.Black);
+            }
+            Result res = new Result(); //Создаем экземпляр формы
+            res.WindowState = FormWindowState.Maximized;
+            image = new Bitmap(bmp);
+            res.ShowDialog(); //Или так
+
+            
+        }
+        public static Bitmap image;
+    }
+}
